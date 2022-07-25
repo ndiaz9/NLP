7383637cf10d5723e279f935139da9c9773faaeb
Arturo Hurtado <spoilers1221@gmail.com>
2022-07-25 09:17:27 -0500

    Gradio multiple model choices

--- Proyecto-G2/gradio_interface.py
+++ Proyecto-G2/gradio_interface.py
@@ -1,13 +1,22 @@
 import gradio as gr
-from sklearn.preprocessing import Normalizer
 import pickle
+from gradio.components import Dropdown, Textbox
+from sklearn.preprocessing import Normalizer
 
-def predictor(text):
-    count_vector = pickle.load(open('salida/naive_bayes.vector', 'rb'))
-    naive_bayes = pickle.load(open('salida/naive_bayes.model', 'rb'))
-    data = count_vector.transform([text])
-    norm = Normalizer().fit_transform(data)
-    return 'Hate Speech' if naive_bayes.predict(norm)[0] == 1 else 'Not Hate Speech'
+def predictor(model, text):
+    if model == "Naive Bayes":
+        count_vector = pickle.load(open('salida/naive_bayes.vector', 'rb'))
+        naive_bayes = pickle.load(open('salida/naive_bayes.model', 'rb'))
+        data = count_vector.transform([text])
+        norm = Normalizer().fit_transform(data)
+        response = 'Hate Speech' if naive_bayes.predict(norm)[0] == 1 else 'Not Hate Speech'
+    else:
+        response = "Modelo no disponible"
+    return response
 
-iface = gr.Interface(fn=predictor, inputs="text", outputs="text")
+input1 = Dropdown(["Naive Bayes", "Logistic Regression", "Transformers"], label="Model")
+input2 = Textbox(placeholder="Enter Phrase", label="Phrase")
+output = Textbox(label="Result")
+
+iface = gr.Interface(fn=predictor, inputs=[input1, input2], outputs=[output], live=True,)
 iface.launch()
